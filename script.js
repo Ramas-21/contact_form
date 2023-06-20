e81325a2b905fb8db92be4013703b5a7a0aa108b
Lawrence <lawrencehome21@gmail.com>
2023-06-20 08:41:50 -0700

    added an if statement

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ statusTxt = form.querySelector(".button-area span");
 
 form.onsubmit = (e) => {
     e.preventDefault(); // preventing form from submitting
+    statusTxt.style.color = "#0D6EfD";
     statusTxt.style.display = "block";
 
     let xhr = new XMLHttpRequest(); // creating new xml object
@@ -12,7 +13,15 @@ form.onsubmit = (e) => {
     xhr.onload = () => {
         if(xhr.readyState == 4 && xhr.status == 200) {
             let response = xhr.response;
-            console.log(response);
+            if(response.indexOf("Email and name field is required") != -1 || response.indexOf("Enter a valid email address") || response.indexOf("Sorry failed to send your message")){
+                statusTxt.style.color = "red";
+            }
+            else{
+                form.reset();
+                setTimeout(() =>{
+                    statusTxt.style.display = "none";
+                }, 3000);
+            }
             statusTxt.innerText = response;
         }
     }
